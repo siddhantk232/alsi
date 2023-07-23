9791dd98d0f63225413c859b598f2855d9a28174
siddhantCodes <siddhantk232@gmail.com>
2023-07-23 21:25:35 +0530

    bubble sort

--- main.c
+++ main.c
@@ -6,37 +6,15 @@
 int const WIDTH = 1280;
 int const HEIGHT = 720;
 
-void bubble_sort(int arr[], int size) {
-    for (int i = 0; i < size - 1; ++i) {
-        for (int j = 0; j < size - i - 1; ++j) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-}
-
 int main() {
-    int arr[] = {20, -1, 2, 1, 14, 12, 0, 1, 2, -5, 99};
-    int size = sizeof(arr) / sizeof(int);
-
-    bubble_sort(arr, size);
-
-    for (int i = 0; i < size; ++i) {
-        printf("%d ", arr[i]);
-    }
-
-    printf("\n");
-
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window *window;
-    SDL_Renderer *renderer;
+    SDL_Window *window =
+        SDL_CreateWindow("ALSI", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                         WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
 
-    SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer);
-    SDL_SetWindowTitle(window, "ALSI");
+    SDL_Renderer *renderer = SDL_CreateRenderer(
+        window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
 
     if (window == NULL) {
         printf("[ERROR]: failed to initialize window : %s\n", SDL_GetError());
@@ -49,8 +27,17 @@ int main() {
 
     bool is_running = true;
 
+    int arr[] = {100, 200, 150, 250, 300, 280, 370, 280, 650, 480, 390};
+    size_t size = sizeof(arr) / sizeof(int);
+
+    const int bar_width = (WIDTH - (size * 2)) / size;
+
+    size_t i = 0;
+    size_t j = 0;
+
     while (is_running) {
         SDL_Event event;
+        int x = 0;
 
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
@@ -64,18 +51,69 @@ int main() {
         }
 
         SDL_RenderClear(renderer);
-        SDL_Rect rect = {
-            .h = 200,
-            .w = 200,
-            .x = WIDTH / 2 - 100,
-            .y = HEIGHT / 2 - 100,
-        };
-        SDL_SetRenderDrawColor(renderer, 255, 155, 0, 255);
-        SDL_RenderFillRect(renderer, &rect);
+        bool swap = false;
+        if (i < size) {
+            if (j < size - i - 1) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swap = true;
+                }
+            }
+        }
+
+        for (size_t k = 0; k < size; ++k) {
+            SDL_Rect rect = {
+                .h = arr[k],
+                .w = bar_width,
+                .x = x,
+                .y = HEIGHT - arr[k],
+            };
+
+            if (j == k) {
+                SDL_SetRenderDrawColor(renderer, 200, 155, 0, 255);
+            } else {
+                SDL_SetRenderDrawColor(renderer, 255, 155, 0, 255);
+            }
+
+            if (swap && j + 1 == k) {
+                SDL_SetRenderDrawColor(renderer, 50, 155, 0, 255);
+            }
+
+            SDL_RenderFillRect(renderer, &rect);
+            x += bar_width;
+
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            SDL_Rect sep = {
+                .h = HEIGHT,
+                .w = 2,
+                .x = x,
+                .y = 0,
+            };
+
+            SDL_RenderFillRect(renderer, &sep);
+            x += 2;
+        }
+
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderPresent(renderer);
+
+        j++;
+
+        if (j == size - 1) {
+            i++;
+            j = 0;
+        }
+
+        // TODO: show a finish message (text rendering)
+        if (i == size - 1) break;
+
+        SDL_Delay(300);
     }
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    SDL_Quit();
 }
